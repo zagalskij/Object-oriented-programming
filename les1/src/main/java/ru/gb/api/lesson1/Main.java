4a0cf3ba64d44eeb800f1c7a0d2bef4c6394cf17
Anatoliy <zaghalskiy@mail.ru>
2023-09-04 12:54:01 +0300

    first task

---
+++ les1/src/main/java/ru/gb/api/lesson1/Main.java
@@ -0,0 +1,10 @@
+package ru.gb.api.lesson1;
+
+public class Main {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat();
+        cat1.name = "Барсик";
+        cat1.color = "Рыжий";
+        System.out.println("cat1 = " + cat1);
+    }
+}
