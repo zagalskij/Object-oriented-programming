b14226a57ecf0505c134856336964250ec076e0e
Anatoly <zaghalskiy@mail.ru>
2023-10-13 17:32:20 +0300

    complte task1(1) homework3

--- src/main/java/ru/gb/api/homework3/Stream.java
+++ src/main/java/ru/gb/api/homework3/Stream.java
@@ -1,5 +1,36 @@
 package ru.gb.api.homework3;
 
-public class Flow {
-    study groups
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Stream implements  Iterable<Integer>{
+    private String streamName;
+    private List<Group> listGroup;
+
+    public String getStreamName() {
+        return streamName;
+    }
+
+    public void setStreamName(String streamName) {
+        this.streamName = streamName;
+    }
+
+    public List<Group> getListGroup() {
+        return listGroup;
+    }
+
+    public void setListGroup(List<Group> listGroup) {
+        this.listGroup = listGroup;
+    }
+
+    public Stream(String streamName, List<Group> listGroup) {
+        this.streamName = streamName;
+        this.listGroup = listGroup;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new StreamIterator(this);
+    }
 }
--- src/main/java/ru/gb/api/homework3/Student.java
+++ src/main/java/ru/gb/api/homework3/Student.java
@@ -1,2 +1,21 @@
-package ru.gb.api.homework3;public class Student {
+package ru.gb.api.homework3;
+
+public class Student {
+    private String name;
+    private int studentId;
+    private int age;
+    private String major;
+    private String email;
+    private double gpa;
+    private String address;
+
+    public Student(String name, int studentId, int age, String major, String email, double gpa, String address) {
+        this.name = name;
+        this.studentId = studentId;
+        this.age = age;
+        this.major = major;
+        this.email = email;
+        this.gpa = gpa;
+        this.address = address;
+    }
 }
